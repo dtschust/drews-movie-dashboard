8e5d3ba29b8a5a630c3ca03e655e5d5dc2f6ade9
Drew Schuster <drewschuster@openai.com>
2025-10-10 14:25:12 -0700

    tsc fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 import { resolve } from 'path';
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
   const modeIsEmbedded = mode === 'embedded' ? true : mode === 'web' ? false : undefined;
   const isEmbedded = modeIsEmbedded ?? envIsEmbedded ?? false;
 
-  const plugins = [react()];
+  const plugins: PluginOption[] = [react()];
   if (isEmbedded) {
     plugins.push(viteSingleFile());
   }
