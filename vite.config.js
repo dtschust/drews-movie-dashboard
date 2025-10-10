2ac1569a1c9d1fd148e001eb45bf5f271b3c7161
Drew Schuster <dtschust@gmail.com>
2025-10-09 21:43:04 -0700

    bugfix for esm build

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
 
-// GitHub Pages base path (repo name). Adjust if deploying to a different path.
-const base = '/drews-movie-dashboard/';
-
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-  base,
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
